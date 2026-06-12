062f9a3812b773b49721b5c489199c2df2f1348a
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-06-12 13:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function toCase(text) {
   // write your code here
+	let t=text;
+	return text+'-'+t.toUpperCase();
 }
 
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
